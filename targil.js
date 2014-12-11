d66b70c7741e22340db7a25ce2a4709c6dc21708
itzikbe13 <itzikbe13@gmail.com>
2014-12-11 05:40:36 +0200

    Create targil.js

---
+++ targil.js
@@ -0,0 +1,281 @@
+console.log("Welcome");
+$(document).ready(function(){
+
+ //sunday morning
+ var m = $("button:nth(0)").on("click", function(){
+	if($("button:nth(0)").val() === ''  && $("button:nth(7)").val() === '' &&  $("button:nth(14)").val() === '' && $("button:nth(21)").val() === '' && $("button:nth(21)").val() === '') {
+	$("button:nth(0)").val('1') 
+	$("button:nth(0)").css("background-color", "red");
+	} else {
+	$("button:nth(0)").val("")
+	$("button:nth(0)").css("background-color", "white");
+	}
+	});
+	
+
+	
+	//sunday middle
+ var m = $("button:nth(7)").on("click", function(){
+	if($("button:nth(0)").val() === ''  && $("button:nth(7)").val() === '' &&  $("button:nth(14)").val() === '' && $("button:nth(21)").val() === '') {
+	$("button:nth(7)").val('1') 
+	$("button:nth(7)").css("background-color", "red");
+	} else {
+	$("button:nth(7)").val("")
+	$("button:nth(7)").css("background-color", "white");
+	}
+	});
+
+	
+	//sunday evening
+	 var m = $("button:nth(14)").on("click", function(){
+	if($("button:nth(0)").val() === ''  && $("button:nth(7)").val() === '' &&  $("button:nth(14)").val() === '' && $("button:nth(21)").val() === '') {
+	$("button:nth(14)").val('1') 
+	$("button:nth(14)").css("background-color", "red");
+	} else {
+	$("button:nth(14)").val("")
+	$("button:nth(14)").css("background-color", "white");
+	}
+	});
+	
+	
+	//monday morning
+ var m = $("button:nth(1)").on("click", function(){
+	if($("button:nth(1)").val() === ''  && $("button:nth(8)").val() === '' &&  $("button:nth(15)").val() === '') {
+	$("button:nth(1)").val('1') 
+	$("button:nth(1)").css("background-color", "red");
+	} else {
+	$("button:nth(1)").val("")
+	$("button:nth(1)").css("background-color", "white");
+	}
+	});
+	
+
+	
+	//monday middle
+ var m = $("button:nth(8)").on("click", function(){
+	if($("button:nth(1)").val() === ''  && $("button:nth(8)").val() === '' &&  $("button:nth(15)").val() === '') {
+	$("button:nth(8)").val('1') 
+	$("button:nth(8)").css("background-color", "red");
+	} else {
+	$("button:nth(8)").val("")
+	$("button:nth(8)").css("background-color", "white");
+	}
+	});
+
+	
+	//monday evening
+	 var m = $("button:nth(15)").on("click", function(){
+	if($("button:nth(1)").val() === ''  && $("button:nth(8)").val() === '' &&  $("button:nth(15)").val() === '') {
+	$("button:nth(15)").val('1') 
+	$("button:nth(15)").css("background-color", "red");
+	} else {
+	$("button:nth(15)").val("")
+	$("button:nth(15)").css("background-color", "white");
+	}
+	});
+	
+		//tuesday morning
+ var m = $("button:nth(2)").on("click", function(){
+	if($("button:nth(2)").val() === ''  && $("button:nth(9)").val() === '' &&  $("button:nth(16)").val() === '') {
+	$("button:nth(2)").val('1') 
+	$("button:nth(2)").css("background-color", "red");
+	} else {
+	$("button:nth(2)").val("")
+	$("button:nth(2)").css("background-color", "white");
+	}
+	});
+	
+
+	
+	//tuesday middle
+ var m = $("button:nth(9)").on("click", function(){
+	if($("button:nth(2)").val() === ''  && $("button:nth(9)").val() === '' &&  $("button:nth(16)").val() === '') {
+	$("button:nth(9)").val('1') 
+	$("button:nth(9)").css("background-color", "red");
+	} else {
+	$("button:nth(9)").val("")
+	$("button:nth(9)").css("background-color", "white");
+	}
+	});
+
+	
+	//tuesday evening
+	 var m = $("button:nth(16)").on("click", function(){
+	if($("button:nth(2)").val() === ''  && $("button:nth(9)").val() === '' &&  $("button:nth(16)").val() === '') {
+	$("button:nth(16)").val('1') 
+	$("button:nth(16)").css("background-color", "red");
+	} else {
+	$("button:nth(16)").val("")
+	$("button:nth(16)").css("background-color", "white");
+	}
+	});
+	
+		//wednesday morning
+ var m = $("button:nth(3)").on("click", function(){
+	if($("button:nth(3)").val() === ''  && $("button:nth(10)").val() === '' &&  $("button:nth(17)").val() === '') {
+	$("button:nth(3)").val('1') 
+	$("button:nth(3)").css("background-color", "red");
+	} else {
+	$("button:nth(3)").val("")
+	$("button:nth(3)").css("background-color", "white");
+	}
+	});
+	
+
+	
+	//wednesday middle
+ var m = $("button:nth(10)").on("click", function(){
+	if($("button:nth(3)").val() === ''  && $("button:nth(10)").val() === '' &&  $("button:nth(17)").val() === '') {
+	$("button:nth(10)").val('1') 
+	$("button:nth(10)").css("background-color", "red");
+	} else {
+	$("button:nth(10)").val("")
+	$("button:nth(10)").css("background-color", "white");
+	}
+	});
+
+	
+	//wednesday evening
+	 var m = $("button:nth(17)").on("click", function(){
+	if($("button:nth(3)").val() === ''  && $("button:nth(10)").val() === '' &&  $("button:nth(17)").val() === '') {
+	$("button:nth(17)").val('1') 
+	$("button:nth(17)").css("background-color", "red");
+	} else {
+	$("button:nth(17)").val("")
+	$("button:nth(17)").css("background-color", "white");
+	}
+	});
+	
+		//thursday morning
+ var m = $("button:nth(4)").on("click", function(){
+	if($("button:nth(4)").val() === ''  && $("button:nth(11)").val() === '' &&  $("button:nth(18)").val() === '') {
+	$("button:nth(4)").val('1') 
+	$("button:nth(4)").css("background-color", "red");
+	} else {
+	$("button:nth(4)").val("")
+	$("button:nth(4)").css("background-color", "white");
+	}
+	});
+	
+
+	
+	//thursday middle
+ var m = $("button:nth(11)").on("click", function(){
+	if($("button:nth(4)").val() === ''  && $("button:nth(11)").val() === '' &&  $("button:nth(18)").val() === '') {
+	$("button:nth(11)").val('1') 
+	$("button:nth(11)").css("background-color", "red");
+	} else {
+	$("button:nth(11)").val("")
+	$("button:nth(11)").css("background-color", "white");
+	}
+	});
+
+	
+	//thursday evening
+	 var m = $("button:nth(18)").on("click", function(){
+	if($("button:nth(4)").val() === ''  && $("button:nth(11)").val() === '' &&  $("button:nth(18)").val() === '') {
+	$("button:nth(18)").val('1') 
+	$("button:nth(18)").css("background-color", "red");
+	} else {
+	$("button:nth(18)").val("")
+	$("button:nth(18)").css("background-color", "white");
+	}
+	});
+	
+		//friday morning
+ var m = $("button:nth(5)").on("click", function(){
+	if($("button:nth(5)").val() === ''  && $("button:nth(12)").val() === '' &&  $("button:nth(19)").val() === '') {
+	$("button:nth(5)").val('1') 
+	$("button:nth(5)").css("background-color", "red");
+	} else {
+	$("button:nth(5)").val("")
+	$("button:nth(5)").css("background-color", "white");
+	}
+	});
+	
+
+	
+	//friday middle
+ var m = $("button:nth(12)").on("click", function(){
+	if($("button:nth(5)").val() === ''  && $("button:nth(12)").val() === '' &&  $("button:nth(19)").val() === '') {
+	$("button:nth(12)").val('1') 
+	$("button:nth(12)").css("background-color", "red");
+	} else {
+	$("button:nth(12)").val("")
+	$("button:nth(12)").css("background-color", "white");
+	}
+	});
+
+	
+	//friday evening
+	 var m = $("button:nth(19)").on("click", function(){
+	if($("button:nth(5)").val() === ''  && $("button:nth(12)").val() === '' &&  $("button:nth(19)").val() === '') {
+	$("button:nth(19)").val('1') 
+	$("button:nth(19)").css("background-color", "red");
+	} else {
+	$("button:nth(19)").val("")
+	$("button:nth(19)").css("background-color", "white");
+	}
+	});
+	
+		//saturday morning
+ var m = $("button:nth(6)").on("click", function(){
+	if($("button:nth(6)").val() === ''  && $("button:nth(13)").val() === '' &&  $("button:nth(20)").val() === '') {
+	$("button:nth(6)").val('1') 
+	$("button:nth(6)").css("background-color", "red");
+	} else {
+	$("button:nth(6)").val("")
+	$("button:nth(6)").css("background-color", "white");
+	}
+	});
+	
+
+	
+	//saturday middle
+ var m = $("button:nth(13)").on("click", function(){
+	if($("button:nth(6)").val() === ''  && $("button:nth(13)").val() === '' &&  $("button:nth(20)").val() === '') {
+	$("button:nth(13)").val('1') 
+	$("button:nth(13)").css("background-color", "red");
+	} else {
+	$("button:nth(13)").val("")
+	$("button:nth(13)").css("background-color", "white");
+	}
+	});
+
+	
+	//saturday evening
+	 var m = $("button:nth(20)").on("click", function(){
+	if($("button:nth(6)").val() === ''  && $("button:nth(13)").val() === '' &&  $("button:nth(20)").val() === '') {
+	$("button:nth(20)").val('1') 
+	$("button:nth(20)").css("background-color", "red");
+	} else {
+	$("button:nth(20)").val("")
+	$("button:nth(20)").css("background-color", "white");
+	}
+	});
+	
+			//hofashim-sunday
+	var m = $("button:nth(21)").on("click", function(){
+	if($("button:nth(0)").val() === ''  && $("button:nth(7)").val() === '' &&  $("button:nth(14)").val() === '' &&  $("button:nth(21)").val() === '') {
+	$("button:nth(21)").val('1'); 
+	$("button:nth(21)").css("background-color", "blue");
+	}
+	else if ($("button:nth(21)").val() === '1' ){
+	$("button:nth(21)").val('2');
+	$("button:nth(21)").css("background-color", "green");
+	} 
+	
+	else if ($("button:nth(21)").val() === '2') {
+		$("button:nth(21)").css("background-color", "white");
+		$("button:nth(21)").val('');
+		}
+	 
+	});
+	
+
+	
+
+	});
+
+	
+	
